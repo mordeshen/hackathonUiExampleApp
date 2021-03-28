5ba0bbb6e87f26f8ffae5b2c00a4a38ce6808a48
Mordechay Shenvald <Mordechay.shenvald@gmail.com>
2021-03-29 00:00:37 +0300

    update

---
+++ app/src/main/java/com/example/hackathonuiex/ui/login/CreateAccountFirstActivity.kt
@@ -0,0 +1,7 @@
+package com.example.hackathonuiex.ui.login
+
+import androidx.fragment.app.Fragment
+import com.example.hackathonuiex.R
+
+class CreateAccountFirstActivity:Fragment(R.layout.activity_create_account_first) {
+}
